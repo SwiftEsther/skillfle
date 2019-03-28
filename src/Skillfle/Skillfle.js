b894e8770ebc6d319e374e70b3b09933dfa05469
Esther Akinloose <swiftestherduro@gmail.com>
2019-03-28 23:08:49 +0100

    How it works section

--- src/Skillfle/Skillfle.js
+++ src/Skillfle/Skillfle.js
@@ -2,11 +2,13 @@ import React, {Fragment} from 'react';
 import Header from '../Components/Header/Header';
 import Institutions from '../Components/Institutions/Institutions';
 import Footer from '../Components/Footer/Footer';
+import Steps from '../Components/Steps/Steps';
 
 const SkillfleApp = () => (
     <Fragment>
         <Header />
         <Institutions />
+        <Steps />
         <Footer />
     </Fragment>
 )
