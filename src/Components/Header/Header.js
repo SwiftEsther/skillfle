2184b893b0fd65c90e1952ae9689a2c5aae7e4c5
Esther Akinloose <swiftestherduro@gmail.com>
2019-04-06 18:00:55 +0100

    Clean-ups

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -14,17 +14,6 @@ const Header = () => (
                 <a href="#signup">Sign Up</a>
             </div>
         </div>
-        {/* <div className="header">
-            <Row>
-                <a href="#" className="logo"><img src={logo} alt="SKillfle Logo"/></a>
-                <ul class="main-nav">
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#company">Company</a></li>
-                    <li><a href="#connect">Connect</a></li>
-                    <li><a href="#plans">Sign up</a></li>
-                </ul>
-            </Row>
-        </div> */}
         <div className="landing_page">
             <div className="hero-text-box">
                 <h1>Free Skills to careers</h1>
